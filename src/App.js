492956fad4be20eb4812d142e5ffbfb9c69d8c03
jack <21058045@imail.sunway.edu.my>
2024-08-20 19:13:04 +0800

    A

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <h1>Hello, User!</h1>
-          <p>Welcome to GBP ImpactXChange Quiz App</p>
+          <h1>Hello, React!</h1>
+          <p>This is my first React app.</p>
         </header>
       </div>
   );
